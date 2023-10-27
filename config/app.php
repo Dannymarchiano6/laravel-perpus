9285def3b346f9ef2346eef03d241dc918201c70
Dannymarchiano6 <dannymarchiano6@gmail.com>
2023-10-27 11:23:19 +0700

    test

--- config/app.php
+++ config/app.php
@@ -3,7 +3,13 @@
 use Illuminate\Support\Facades\Facade;
 use Illuminate\Support\ServiceProvider;
 
+
+
+
+
+
 return [
+
 
     /*
     |--------------------------------------------------------------------------
@@ -168,7 +174,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
     ])->toArray(),
+
 
     /*
     |--------------------------------------------------------------------------
